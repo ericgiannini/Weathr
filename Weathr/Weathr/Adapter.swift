a3c6c489cd27e536abe28fa22b9cbe5645b91ba0
ericgiannini <eric.giannini@yahoo.com>
2016-12-05 15:40:13 -0800

    Adding the Adapter class

--- Weathr/Weathr/Adapter.swift
+++ Weathr/Weathr/Adapter.swift
@@ -27,3 +27,39 @@ public enum Result<T> {
         self = .failure(error)
     }
 }
+
+public class Adapter {
+    func makeRequest(method: HTTPMethod, path: String, headers: [String:String]?, payload: [String:Any]?, handler: @escaping (Result<[String:Any]>) -> ()) throws -> URLSessionDataTask {
+        var request = URLRequest(url: URL(string: "https:\(path)")!)
+        request.allHTTPHeaderFields = headers
+        request.httpMethod = method.rawValue
+        
+        if let payload = payload {
+            request.httpBody = try JSONSerialization.data(withJSONObject: payload, options: [])
+        }
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                handler(Result(error: error))
+                return
+            }
+            
+            if let data = data {
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] {
+                        handler(Result(value: json))
+                        return
+                    }
+                } catch let error {
+                    handler(Result(error: error))
+                    return
+                }
+            }
+            
+            fatalError("should not ever occur.") // We are ignoring some theoretical error cases here
+        }
+        
+        task.resume()
+        return task
+    }
+}
